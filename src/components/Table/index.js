28b8b05026c592705c3f1d3fcc811a7b9ed20022
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-06-05 23:33:44 -0500

    added names to table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -5,26 +5,43 @@ function Table() {
   return <table class="uk-table uk-table-hover uk-table-divider">
   <thead>
       <tr>
-          <th>Table Heading</th>
-          <th>Table Heading</th>
-          <th>Table Heading</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Position</th>
+          <th>Number</th>
       </tr>
   </thead>
   <tbody>
       <tr>
-          <td>Table Data</td>
-          <td>Table Data</td>
-          <td>Table Data</td>
+          <td>Daniel Duran</td>
+          <td>Daniel@gmail.com</td>
+          <td>Web Developer</td>
+          <td>210-222-2222</td>
+          
       </tr>
       <tr>
-          <td>Table Data</td>
-          <td>Table Data</td>
-          <td>Table Data</td>
+          <td>Rick Roll</td>
+          <td>Rick@yahoo.com</td>
+          <td>Web Developer</td>
+          <td>956-444-4444</td>
       </tr>
       <tr>
-          <td>Table Data</td>
-          <td>Table Data</td>
-          <td>Table Data</td>
+          <td>James Dean</td>
+          <td>James@gmail.com</td>
+          <td>Engineer</td>
+          <td>210-888-8888</td>
+      </tr>
+      <tr>
+          <td>Sam witty</td>
+          <td>Sam@gmail.com</td>
+          <td>Engineer</td>
+          <td>210-999-8228</td>
+      </tr>
+      <tr>
+          <td>Charles latts</td>
+          <td>Charles@yahoo.com</td>
+          <td>Accountant</td>
+          <td>956-333-8888</td>
       </tr>
   </tbody>
 </table>;
