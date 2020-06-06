0743c0fb3b2463b303e2e75732d5395c8239e62b
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-06-05 23:46:24 -0500

    created tablerow component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,6 +2,40 @@ import React from "react";
 import "./style.css";
 
 function Table() {
+
+const employees = [
+        {
+            name: "Daniel Duran",
+            email:"Daniel@gmail.com",
+            position: "Web Developer",
+            number: "210-222-2222"
+        },
+        {
+            name: "Rick Roll",
+            email:"Rick@yahoo.com",
+            position: "Web Developer",
+            number: "956-444-4444"
+        },
+        {
+            name: "James Dean",
+            email:"James@gmail.com",
+            position: "Engineer",
+            number: "210-888-8888"
+        },
+        {
+            name: "Sam Smitty",
+            email:"Sam@gmail.com",
+            position: "Engineer",
+            number: "210-999-8228"
+        },
+        {
+            name: "Charles latts",
+            email:"Charles@yahoo.com",
+            position: "Accountant",
+            number: "956-333-888"
+        },
+        
+    ]
   return <table class="uk-table uk-table-hover uk-table-divider">
   <thead>
       <tr>
